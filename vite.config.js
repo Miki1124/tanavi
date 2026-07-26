ab4f3dc4f2af3655034111f2e331ef4850a51570
近都 水紀 <miki-mikan@y-mobile.ne.jp>
2026-07-26 23:18:48 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: '田なび',
+        short_name: '田なび',
+        description: '同志社大学京田辺キャンパスナビ',
+        theme_color: '#7B61FF',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
